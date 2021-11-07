6412da43ca853a8747f1571c47cb7651b501c914
Josh Perry <joshperry@outlook.com>
2021-11-07 23:39:50 +0000

    Added projects list dropdown

--- Frontend/aelia/src/app/navbar/navbar.component.ts
+++ Frontend/aelia/src/app/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProjectService } from '../projects.service';
+import { Project } from '../project';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+  projects: Project[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private projectService: ProjectService) {
   }
 
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(): void {
+    this.projectService.getProjects().subscribe(p => this.projects = p);
+  }
 }
